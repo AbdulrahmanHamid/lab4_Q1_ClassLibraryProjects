e221fc2f8fed9c570148bf27f225e04ec8694fab
AbdulrahmanHamid <abooym@gmail.com>
2026-04-10 01:25:25 -0400

    Initial commit added StringExtensions

---
+++ StringExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace lab4_Q1_ClassLibraryProjects
+{
+    public static class StringExtensions
+    {
+        public static bool StartsWithUpperCase(this string str)
+        {
+            if (string.IsNullOrWhiteSpace(str))
+            {
+                return false;
+            }
+
+            return char.IsUpper(str[0]);
+        }
+    }
+}
